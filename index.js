9cbbc8c00b69df46ebe3afbc6f2c838d294fcdea
Kylie Clark <kyliedarlene@icloud.com>
2024-01-01 15:52:09 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // code your solution here
+function superbowlWin(array) {
+    const winYear = array.find((element) => element.result === "W");
+    if (winYear !== undefined) {
+        return winYear.year;
+    } 
+}
